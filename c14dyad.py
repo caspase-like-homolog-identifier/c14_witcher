6f82b7c5da19f3d7cbc2e5fd366c6951329d22f1
Andrew Ndhlovu <drewxdvst@outlook.com>
2020-12-22 01:58:22 +0200

    finally add p20 and p10 regex

--- c14dyad.py
+++ c14dyad.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 from Bio.ExPASy import Prosite
-from Bio import AlignIO
+from Bio import SeqIO
 import pattern
 import re
-
 
 # {PS01121; CASPASE_HIS}
 # {PS01122; CASPASE_CYS}
@@ -14,23 +13,18 @@ class PrositePattern(object):
 
     def __init__(self, prosite_db = "prosite.dat"):
 
-        prosite_handle = open(prosite_db)
-        prosite_records = Prosite.parse(prosite_handle)
-        prosite_dict = dict([ (rec.accession, rec) for rec in prosite_records ])
-
-        self.HIS = prosite_dict["PS01121"]
-        self.CYS = prosite_dict["PS01122"]
-        
+        #H-x(2,4)-[SC]-x(2)-{A}-x-[LIVMF](2)-[ST]-H-Ge
+        #K-P-K-[LIVMF]-[LIVMFY]-[LIVMF](2)-[QP]-[AF]-C-[RQG]-[GE].
+        self.CYS.pattern = "K-P-K-[LIVMF]-[LIVMFY]-[LIVMF](2)-[QP]-[AF]-C-[RQG]-[GE]."
+        self.CYS.pattern = "[STAVK]-[HTRKP]-[KIMLV]-[LIVMFHT]-[LIVMFYSCA]-[SLIVMF](2)-[DQP]-[TCSAF]-C-[RHQG]-[SGE]"
+    
         self.regex_HIS = re.compile(pattern.prosite_to_re(self.HIS.pattern))
         self.regex_CYS = re.compile(pattern.prosite_to_re(self.CYS.pattern))
-                
-
-        
+                    
     def Caspase_HIS(self, seq):
         return self.regex_HIS.findall(seq)
 
 
-    
     def Caspase_CYS(self, seq):
         return self.regex_CYS.findall(seq)
 
@@ -38,11 +32,26 @@ class PrositePattern(object):
 
 if  __name__ ==  '__main__':
     prosite = PrositePattern()
-    records = AlignIO.parse("in_file_trim.ali","stockholm")
-    alignments = next(records)
-    for aln in alignments:
-        seq = str(aln.seq.ungap("-").upper())
-        print(prosite.Caspase_CYS(seq))
+    records = SeqIO.parse("MCA.fasta","fasta")
+    #records = SeqIO.parse("PF00656_seed_align.txt","fasta")
+    j = 0
+    for i,rec in enumerate(records, 1):
+        find = prosite.Caspase_CYS(str(rec.seq.ungap('-')))
+        
+        print(i, j, rec.id, find)
+        
+        if find:
+            j += 1
+            
+    print("\n\n{:.2%} ({}/{})\n".format(j/float(i),j,i))
+            
+
+        
+    #alignments = next(records)
+    #for aln in alignments:
+        #print(aln)
+        #seq = str(aln.seq.ungap("-").upper())
+        #print(prosite.Caspase_CYS(seq))
     #prosite.
     #https://docs.python.org/3/library/re.html
     #print(pattern.prosite_to_re(self.HIS))
