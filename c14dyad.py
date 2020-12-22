6dc9674b14003a0c728f689a858cff7412bacd05
Andrew Ndhlovu <drewxdvst@outlook.com>
2020-12-22 16:01:16 +0200

    stand alone script for idenfying c14 hys and cis

--- c14dyad.py
+++ c14dyad.py
@@ -1,60 +1,49 @@
 #!/usr/bin/env python
 from Bio.ExPASy import Prosite
-from Bio import SeqIO
+import argparse
 import pattern
 import re
 
-# {PS01121; CASPASE_HIS}
-# {PS01122; CASPASE_CYS}
-# {PS50207; CASPASE_P10}
-# {PS50208; CASPASE_P20}
+
 
 class PrositePattern(object):
 
-    def __init__(self, prosite_db = "prosite.dat"):
+    def __init__(self):
 
-        #H-x(2,4)-[SC]-x(2)-{A}-x-[LIVMF](2)-[ST]-H-Ge
-        #K-P-K-[LIVMF]-[LIVMFY]-[LIVMF](2)-[QP]-[AF]-C-[RQG]-[GE].
-        self.CYS.pattern = "K-P-K-[LIVMF]-[LIVMFY]-[LIVMF](2)-[QP]-[AF]-C-[RQG]-[GE]."
-        self.CYS.pattern = "[STAVK]-[HTRKP]-[KIMLV]-[LIVMFHT]-[LIVMFYSCA]-[SLIVMF](2)-[DQP]-[TCSAF]-C-[RHQG]-[SGE]"
-    
-        self.regex_HIS = re.compile(pattern.prosite_to_re(self.HIS.pattern))
-        self.regex_CYS = re.compile(pattern.prosite_to_re(self.CYS.pattern))
-                    
+        """instatiates prosite pattern search"""
+        
+        #https://prosite.expasy.org/PS01121
+        self.HIS_pattern = "H(0,1)-x(2,4)-[SCQ](0,1)-x(2)-{A}-x-[LIVMF](0,3)-[HFYST](0,2)-[GST](1,2)-H-G"
+        #https://prosite.expasy.org/PS01122
+        self.CYS_pattern = "[STAVK]-[HTRKP]-[KIMLV]-[LIVMFHT]-[LIVMFYSCA]-[SLIVMF](2)-[DQP]-[TCSAF]-C-[RHQG]-[SGE]"
+
+        self.regex_HIS = re.compile(pattern.prosite_to_re(self.HIS_pattern))
+        self.regex_CYS = re.compile(pattern.prosite_to_re(self.CYS_pattern))
+
+        
     def Caspase_HIS(self, seq):
+        """find p20 HIS residue"""
         return self.regex_HIS.findall(seq)
 
-
+    
     def Caspase_CYS(self, seq):
+        """find p20 HIS residue"""
         return self.regex_CYS.findall(seq)
 
-
-
+    
 if  __name__ ==  '__main__':
+    from Bio import SeqIO
+    parser = argparse.ArgumentParser("Find P20 Cystine and Histidine dyads")
+    parser.add_argument('sequences',help ="sequences file", type=argparse.FileType('r'))
+    parser.add_argument('-f','--format', default = "fasta" ,help ="sequences format")
+    args = parser.parse_args()
+
     prosite = PrositePattern()
-    records = SeqIO.parse("MCA.fasta","fasta")
-    #records = SeqIO.parse("PF00656_seed_align.txt","fasta")
+    records = SeqIO.parse(args.sequences, args.format)
+    
     j = 0
     for i,rec in enumerate(records, 1):
-        find = prosite.Caspase_CYS(str(rec.seq.ungap('-')))
-        
-        print(i, j, rec.id, find)
-        
-        if find:
-            j += 1
-            
-    print("\n\n{:.2%} ({}/{})\n".format(j/float(i),j,i))
-            
-
-        
-    #alignments = next(records)
-    #for aln in alignments:
-        #print(aln)
-        #seq = str(aln.seq.ungap("-").upper())
-        #print(prosite.Caspase_CYS(seq))
-    #prosite.
-    #https://docs.python.org/3/library/re.html
-    #print(pattern.prosite_to_re(self.HIS))
-    #patt = "H-x(2,4)-[SC]-x(2)-{A}-x-[LIVMF](2)-[ST]-H-G"
-    #patt = "H.{2,4}[SC].{2}[^A].[LIVMF]{2}[ST]HG"
-      
+        cys = "".join(prosite.Caspase_CYS(str(rec.seq.ungap('-'))))
+        his = "".join(prosite.Caspase_HIS(str(rec.seq.ungap('-'))))
+        print("{:<20}{:<35}{:<50}".format(rec.id,his,cys))
+    
