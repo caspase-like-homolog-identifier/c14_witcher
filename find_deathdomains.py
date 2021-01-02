48e65f282472acbd4a960ee06697ebae0e2bcbb1
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-03 00:11:43 +0200

    add hmmer parser

--- find_deathdomains.py
+++ find_deathdomains.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from run_hmmer import RunHmmer
+from Bio import SearchIO
+import tempfile
 import argparse
 import glob
 
@@ -8,11 +10,33 @@ import glob
 class FindDeathDomains(RunHmmer):
 
      def __init__(self, seqfile, dd_hmm_path, *hmmersearch_args):
-         
+
+         """ Subclass the Hmmer commandline wrapper """ 
+          
          self.dd_hmm_paths = glob.glob(dd_hmm_path)
-         super().__init__("hmmsearch", None, seqfile, None, None, *args)
+         super().__init__("hmmsearch", None, seqfile, None, None, *hmmersearch_args)
          
-         
+
+     def iterater_deathdomains(self):
+
+          """ iterate over the deathdomains """
+
+          for hmm_file in self.dd_hmm_paths:
+               self.hmmfile = hmm_file
+               tmp1, tmp2 = [ tempfile.NamedTemporaryFile(delete=False) for _ in range(2) ]
+               self.align_out = tmp1.name
+               self.domtblout = tmp2.name
+               print(self.hmmfile)
+               std, stderr = self()
+               print(self.has_deathdomain(self.domtblout))
+               
+
+     def has_deathdomain(self, domtab):
+
+          return list(SearchIO.parse(domtab, "hmmsearch3-domtab"))
+
+          
+          
 
 
 
@@ -22,4 +46,4 @@ if __name__ == '__main__':
     parser.add_argument('-g','--hmm_glob', default="/opt/DB_REF/Pfam/Ig*hmm")
     args = parser.parse_args()    
     dd = FindDeathDomains(args.seqfile, args.hmm_glob)
-    
+    dd.iterater_deathdomains()
