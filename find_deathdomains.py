1524585a877b29c61a59620f0de164b8d10d5da4
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-04 00:55:20 +0200

    iterates over other death domains such immunoglobulins

--- find_deathdomains.py
+++ find_deathdomains.py
@@ -2,6 +2,7 @@
 
 from run_hmmer import RunHmmer
 from Bio import SearchIO
+import collections
 import tempfile
 import argparse
 import pprint
@@ -19,7 +20,7 @@ class FindDeathDomains(RunHmmer):
          self.deathdomain_hits = {}
          
 
-     def get_deathdomains(self):
+     def deathdomains_iter(self):
 
           """ iterate over the deathdomains """
 
@@ -33,28 +34,36 @@ class FindDeathDomains(RunHmmer):
                deathdomain = self.has_deathdomain(self.domtblout)
                
                if deathdomain:
-                   self.deathdomain_hits[deathdomain[0]] = deathdomain[0].hits 
-                   
-                    
-               
+                   self.deathdomain_hits[deathdomain[0].id] = deathdomain[0].hits 
 
+                   
      def has_deathdomain(self, domtab):
 
           return list(SearchIO.parse(domtab, "hmmsearch3-domtab"))
 
 
+     @property
      def deathdomains(self):
 
+
+          #print(self.deathdomain_hits)
           if not self.deathdomain_hits:
                self.get_deathdomains()
+          #create dict using seq.ids as keys and empty lists as values
+          dd_dict = collections.defaultdict(list)
+          for dd in self.deathdomain_hits:
+             for hit in self.deathdomain_hits[dd]:
+                  dd_dict[hit.id].append(dd)
+                  
+          return dd_dict                    
           
-
-
-
+     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
     parser.add_argument('seqfile', action='store', type=str)
     parser.add_argument('-g','--hmm_glob', default="/opt/DB_REF/Pfam/Ig*hmm")
     args = parser.parse_args()    
     dd = FindDeathDomains(args.seqfile, args.hmm_glob)
-    dd.iterater_deathdomains()
+    dd.deathdomains_iter()
+    dd.deathdomains
+    
