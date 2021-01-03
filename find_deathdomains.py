d05ae7610ceb68e72bc1f810dceb4c7fbd462e7c
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-03 23:57:30 +0200

    parse and report death domains using biopython

--- find_deathdomains.py
+++ find_deathdomains.py
@@ -4,6 +4,7 @@ from run_hmmer import RunHmmer
 from Bio import SearchIO
 import tempfile
 import argparse
+import pprint
 import glob
 
 
@@ -15,9 +16,10 @@ class FindDeathDomains(RunHmmer):
           
          self.dd_hmm_paths = glob.glob(dd_hmm_path)
          super().__init__("hmmsearch", None, seqfile, None, None, *hmmersearch_args)
+         self.deathdomain_hits = {}
          
 
-     def iterater_deathdomains(self):
+     def get_deathdomains(self):
 
           """ iterate over the deathdomains """
 
@@ -26,16 +28,25 @@ class FindDeathDomains(RunHmmer):
                tmp1, tmp2 = [ tempfile.NamedTemporaryFile(delete=False) for _ in range(2) ]
                self.align_out = tmp1.name
                self.domtblout = tmp2.name
-               print(self.hmmfile)
+               #print(self.hmmfile)
                std, stderr = self()
-               print(self.has_deathdomain(self.domtblout))
+               deathdomain = self.has_deathdomain(self.domtblout)
+               
+               if deathdomain:
+                   self.deathdomain_hits[deathdomain[0]] = deathdomain[0].hits 
+                   
+                    
                
 
      def has_deathdomain(self, domtab):
 
           return list(SearchIO.parse(domtab, "hmmsearch3-domtab"))
 
-          
+
+     def deathdomains(self):
+
+          if not self.deathdomain_hits:
+               self.get_deathdomains()
           
 
 
