3b32e20abe1a0a124b56373335d941e007c0868a
Andrew Ndhlovu <drewxdvst@outlook.com>
2021-01-18 23:09:41 +0200

    add option to selection features of domains to report

--- find_deathdomains.py
+++ find_deathdomains.py
@@ -3,11 +3,12 @@
 from run_hmmer import RunHmmer
 from Bio import SearchIO
 import collections
+import random
 import tempfile
 import argparse
 import pprint
 import glob
-
+import sys
 
 class FindDeathDomains(RunHmmer):
 
@@ -23,19 +24,19 @@ class FindDeathDomains(RunHmmer):
      def deathdomains_iter(self):
 
           """ iterate over the deathdomains """
-
+          dd_names  = set()
           for hmm_file in self.dd_hmm_paths:
                self.hmmfile = hmm_file
                tmp1, tmp2 = [ tempfile.NamedTemporaryFile(delete=False) for _ in range(2) ]
                self.align_out = tmp1.name
                self.domtblout = tmp2.name
-               #print(self.hmmfile)
                std, stderr = self()
                deathdomain = self.has_deathdomain(self.domtblout)
-               
-               if deathdomain:
-                   self.deathdomain_hits[deathdomain[0].id] = deathdomain[0].hits 
 
+               if deathdomain:
+                    self.deathdomain_hits[deathdomain[0].id] = deathdomain[0].hits 
+                    dd_names.add(deathdomain[0].id)
+               
                    
      def has_deathdomain(self, domtab):
 
@@ -44,7 +45,6 @@ class FindDeathDomains(RunHmmer):
 
 
      def print_deathdomains(self):
-
      
          if not self.dd_dict:
                return None
@@ -54,20 +54,35 @@ class FindDeathDomains(RunHmmer):
           
           
      
-     @property
-     def DeathDomains(self):
-
-
-          #print(self.deathdomain_hits)
-          if not self.deathdomain_hits:
-               self.deathdomains_iter()
-          #create dict using seq.ids as keys and empty lists as values
-          self.dd_dict = collections.defaultdict(list)
-          for dd in self.deathdomain_hits:
+     def DeathDomains(self, feature):
+         """Property to view the death domains.Start analysis if not done already"""
+         # _id
+         # _id_alt
+         # _query_id
+         # _description
+         # _description_alt
+         # _query_description
+         # attributes
+         # dbxrefs
+         # _items
+         # accession
+         # seq_len
+         # evalue
+         # bitscore
+         # bias
+         
+         if not self.deathdomain_hits:
+              self.deathdomains_iter()
+         #create dict using seq.ids as keys and empty lists as values
+         self.dd_dict = collections.defaultdict(list)
+         for dd in self.deathdomain_hits:
+             #print(dd)
              for hit in self.deathdomain_hits[dd]:
-                  self.dd_dict[hit.id].append(dd)
-                       
-          return self.dd_dict                    
+                 self.dd_dict[hit.id].append(vars(hit)[feature])
+
+         pprint.pprint(pd.DataFrame(self.dd_dict))
+         
+         #return self.dd_dict                    
 
      
      def c14merge(c14seq_dict1, c14seq_dict2):
@@ -94,4 +109,7 @@ if __name__ == '__main__':
     parser.add_argument('-g','--hmm_glob', default="/opt/DB_REF/Pfam/Ig*hmm")
     args = parser.parse_args()    
     dd = FindDeathDomains(args.seqfile, args.hmm_glob)
-    dd.deathdomains_iter() 
+    dd.deathdomains_iter()
+    print("\n\n\n\n")
+    
+    print(dd.DeathDomains('evalue'))
